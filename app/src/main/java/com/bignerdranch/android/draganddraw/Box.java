7fefd0197a549bf3d5e3d87cd9f9d6cc26f8348e
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2018-05-28 17:57:59 +0200

    Save state on rotation

--- app/src/main/java/com/bignerdranch/android/draganddraw/Box.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/Box.java
@@ -1,13 +1,20 @@
 package com.bignerdranch.android.draganddraw;
 
 import android.graphics.PointF;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Box {
+public class Box implements Parcelable {
     private PointF mOrigin;
     private PointF mCurrent;
 
     public Box(PointF origin) {
         mOrigin = origin;
+    }
+
+    private Box(Parcel in) {
+        mOrigin.readFromParcel(in);
+        mCurrent.readFromParcel(in);
     }
 
     public PointF getCurrent() {
@@ -21,4 +28,28 @@ public class Box {
     public PointF getOrigin() {
         return mOrigin;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        mOrigin.writeToParcel(dest, flags);
+        mCurrent.writeToParcel(dest, flags);
+    }
+
+    public static final Parcelable.Creator<Box> CREATOR = new Parcelable.Creator<Box>() {
+        @Override
+        public Box createFromParcel(Parcel source) {
+            Box b = new Box(source);
+            return b;
+        }
+
+        @Override
+        public Box[] newArray(int size) {
+            return new Box[size];
+        }
+    };
 }
--- app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -4,16 +4,22 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BoxDrawingView extends View {
     private static final String TAG = "BOX_DRAWING_VIEW";
+    private static final String STATE_PARENT_KEY = "parent";
+    private static final String STATE_BOXEN_KEY = "boxen";
 
     private Box mCurrentBox;
     private List<Box> mBoxen = new ArrayList<>();
@@ -81,6 +87,33 @@ public class BoxDrawingView extends View {
             float top = Math.min(box.getOrigin().y, box.getCurrent().y);
             float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
             canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
+    }
+
+    @Nullable
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable parent = super.onSaveInstanceState();
+
+        Bundle state = new Bundle();
+        state.putParcelable(STATE_PARENT_KEY, parent);
+        state.putParcelableArray(STATE_BOXEN_KEY, mBoxen.toArray(new Box[mBoxen.size()]));
+
+        Log.i(TAG, "Save state");
+        return state;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state != null && state instanceof Bundle) {
+            Log.i(TAG, "Restore state");
+            Bundle bundle = (Bundle) state;
+            super.onRestoreInstanceState(bundle.getParcelable(STATE_PARENT_KEY));
+            Box[] boxen = (Box[]) bundle.getParcelableArray(STATE_BOXEN_KEY);
+            mBoxen = new ArrayList<>(Arrays.asList(boxen));
+        } else {
+            Log.i(TAG, "Restore state (super)");
+            super.onRestoreInstanceState(state);
         }
     }
 }
